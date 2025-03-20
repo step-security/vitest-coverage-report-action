9f6338f24be1d866d78709a6010818136c372b5a
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-03-20 15:26:06 +0530

    code built

--- esbuild.cjs
+++ esbuild.cjs
@@ -1,11 +1,12 @@
 const { build } = require("esbuild");
 
 build({
-	bundle: true,
-	minify: true,
-	sourcemap: true,
-	platform: "node",
-	entryPoints: ["src/index.ts"],
-	outfile: "dist/index.js",
-	target: "node16",
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  platform: "node",
+  entryPoints: ["src/index.ts"],
+  outfile: "dist/index.js",
+  target: "node16",
+  external: ["@actions/core", "@actions/github", "@octokit/request-error", "axios", "common-tags"],
 }).catch(() => process.exit(1));
